631e349f1e64a15dea933abdc904860cc9146423
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-12-13 21:57:59 +0900

    お問い合わせフォームの実装＆ルーティング設定

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import BlogFunc, CategoryFunc, ListFunc, DetailFunc
+from .views import BlogFunc, CategoryFunc, ListFunc, DetailFunc, ContactView, ContentView, SendEmailView, SuccessFunc
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', BlogFunc, name='top'),
     path('category/<str:category>', CategoryFunc, name='category'),
     path('list/', ListFunc, name='list'),
     path('detail/<int:pk>/', DetailFunc, name='detail'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('contact/content/', ContentView.as_view(), name='content'),
+    path('contact/send_email/', SendEmailView.as_view(), name='send'),
+    path('contact/success/', SuccessFunc, name='success'),
 ]
