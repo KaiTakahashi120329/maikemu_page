b47e0c2ab3fa943feea2155fdff21bfdfc0f7748
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-12-13 21:17:37 +0900

    管理画面でカテゴリーを使えるように設定

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import BaseModel
+from .models import BaseModel, Category
 from django_summernote.admin import SummernoteModelAdmin
 
 class BaseModelAdmin(SummernoteModelAdmin):
     summernote_fields = ('text',)
 
 admin.site.register(BaseModel, BaseModelAdmin)
+admin.site.register(Category)
