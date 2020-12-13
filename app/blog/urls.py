07309794d787e76f55f3d14e1013c9e9ddbf6127
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-12-13 21:41:20 +0900

    /detail/のルーティング設定

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogFunc, CategoryFunc, ListFunc
+from .views import BlogFunc, CategoryFunc, ListFunc, DetailFunc
 
 urlpatterns = [
     path('', BlogFunc, name='top'),
     path('category/<str:category>', CategoryFunc, name='category'),
     path('list/', ListFunc, name='list'),
+    path('detail/<int:pk>/', DetailFunc, name='detail'),
 ]
