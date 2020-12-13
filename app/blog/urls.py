1ddca319b20652789160e000656e2c1a49282eaf
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-12-13 21:38:22 +0900

    /list/のルーティング設定

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogFunc, CategoryFunc
+from .views import BlogFunc, CategoryFunc, ListFunc
 
 urlpatterns = [
     path('', BlogFunc, name='top'),
     path('category/<str:category>', CategoryFunc, name='category'),
+    path('list/', ListFunc, name='list'),
 ]
