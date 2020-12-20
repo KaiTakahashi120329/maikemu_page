c29dc929b658a05bbfa187b409ba2cd10c8edc81
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-12-20 18:31:01 +0900

    コメントの追加

--- app/static/js/list/index.js
+++ app/static/js/list/index.js
@@ -3,12 +3,12 @@ const trigger = document.querySelectorAll('.js--trigger');
 [].slice.call(trigger).forEach(element => {
   element.addEventListener('click', () => {
 
+    // 対象の要素取得
     const firstChildElement = element.firstElementChild;
+    // アイコン要素取得
     const iconElement = firstChildElement.lastElementChild;
+    // 隠れている要素取得
     const lastChildElement = element.lastElementChild;
-    console.log(firstChildElement);
-    console.log(lastChildElement);
-
 
     iconElement.classList.toggle('is-rotate');
     lastChildElement.classList.toggle('is-active');
